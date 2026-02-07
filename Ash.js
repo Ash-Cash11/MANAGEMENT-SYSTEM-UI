b28d49f18b7143186953e54492acb4ad79f7fd07
Ashwani Mishra <ashwanim154@gmail.com>
2026-02-07 10:48:06 +0530

    Add files via upload

---
+++ Ash.js
@@ -0,0 +1,15 @@
+const emailInput=document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit",function(event){
+
+    event.preventDefault();
+    const email=emailInput.value;
+    const password=passwordInput.value;
+
+    console.log("Email:",email);
+    console.log("Password:", password);
+
+
+});
